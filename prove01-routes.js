27e60f48a2fcb3211fbea1f38b2722622ef3f578
sth2468 <60360920+sth2468@users.noreply.github.com>
2021-09-16 15:07:07 -0700

    add style to prove01

--- prove01-routes.js
+++ prove01-routes.js
@@ -6,9 +6,16 @@ const requestHandler = (req, res) => {
   const method = req.method;
   if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>Prove01</title></head>');
+    res.write('<head><title>Prove01</title>');
+    res.write('<style>');
+    res.write('body { background-color: black; color: white; }');
+    res.write('h1 { font-family: "Blackadder ITC", "Curlz MT", "Felix Titling", sans-serif; color: white; font-size: 55pt; text-align: center; padding: 15pt; }');
+    res.write('form { padding: 0 200pt; }');
+    res.write('label { font-size: 20pt; }');
+    res.write('button { font-size: 20pt; margin: 20pt; }');
+    res.write('</style></head>');
     res.write('<body>');
-    res.write('<h1>Welcome to the Wizarding World of Prove01!</h1>');
+    res.write('<h1>Welcome to the Wizarding World of Prove 01!</h1>');
     res.write('<form action="/create-user" method="POST"><label for="username">Username:</label><input type="text" id="username" name="username"><button type="submit">Submit</button></form>');
     res.write('</body></html>');
     return res.end();
@@ -16,13 +23,22 @@ const requestHandler = (req, res) => {
   
   if (url === '/users') {
     res.write('<html>');
-    res.write('<head><title>Dummies</title></head>');
+    res.write('<head><title>Dummies</title>');
+    res.write('<style>');
+    res.write('body { background-color: black; color: white; }');
+    res.write('h1 { font-family: "Blackadder ITC", "Curlz MT", "Felix Titling", sans-serif; color: white; font-size: 55pt; text-align: center; padding: 15pt; }');
+    res.write('ul, form { padding: 0 200pt; }');
+    res.write('li, label { font-size: 20pt; }');
+    res.write('button { font-size: 20pt; margin: 20pt; }');
+    res.write('</style></head>');
     res.write('<body><h1>Prove 01 Wizards & Witches User List:</h1>');
     res.write('<ul>');
     for (const user of users) {
       res.write(`<li>${user}</li>`);
     }
-    res.write('</ul></body></html>');
+    res.write('</ul>');
+    res.write('<form action="/create-user" method="POST"><label for="username">Username:</label><input type="text" id="username" name="username"><button type="submit">Submit</button></form>');
+    res.write('</body></html>');
     return res.end();
   }
 
