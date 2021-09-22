167475b3bca6a31ceb00691a0ce10477643f6f09
sth2468 <60360920+sth2468@users.noreply.github.com>
2021-09-21 22:27:21 -0700

    add ta02 team work

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,40 @@
 const express = require('express');
 const router = express.Router();
 
+const users = new Set(['Tom', 'Brent', 'Sandy', 'Drew', 'Carlos']);
+let errorMsg = null;
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users,
+    errorMsg,
   });
+});
+
+router.post('/addUser', (req, res, next) => {
+  const userName = req.body.username;
+  if (!users.has(userName)) {
+    users.add(userName);
+    errorMsg = null;    
+  } else {
+    errorMsg = `The user, "${userName}", already exists.`
+  }
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const userName = req.body.userlist;
+  if (users.has(userName)) {
+    users.delete(userName);
+    errorMsg = null;    
+  } else {
+    errorMsg = `The user, "${userName}", doesn't exist.`
+  }
+  res.redirect('/ta02');
 });
 
 module.exports = router;
